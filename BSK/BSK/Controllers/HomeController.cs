f92066e66319dc0295fb70f9803ef85edb17cc8d
paulinabukowska <paulinabukowska94@gmail.com>
2016-04-23 22:53:53 +0200

    dodałam logout i początkowy widok

--- BSK/BSK/Controllers/HomeController.cs
+++ BSK/BSK/Controllers/HomeController.cs
@@ -6,23 +6,13 @@ using System.Web.Mvc;
 
 namespace BSK.Controllers
 {
+
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Title = "Home Page";
 
             return View();
         }
---
+++ BSK/BSK/Controllers/LogOutController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+using BSK.Models;
+using System.Net;
+
+namespace BSK.Controllers
+{
+    public class LogOutController : ApiController
+    {
+        public HttpResponseMessage Post(LogOutZapytanie dane)
+        {
+            HttpResponseMessage odpowiedz;
+            using (DB baza = new DB())
+            {
+                if(baza.Sesje.Any(s=>s.ID_Sesji == dane.ID_Sesji))
+                {
+                    Sesja sesja = baza.Sesje.FirstOrDefault(s => s.ID_Sesji == dane.ID_Sesji);
+                    baza.Sesje.Remove(sesja);
+                    baza.SaveChanges();
+                }
+                odpowiedz = Request.CreateResponse(HttpStatusCode.OK);
+            }
+            return odpowiedz;
+        }
+    }
+}
